5bbc089a62aa2484f6b2a04926d2223c752cbf26
Jeff Ripke <jripke74@gmail.com>
2017-09-11 05:42:31 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const form = document.getElementById('registrar');
+const input = form.querySelector('input');
+form.addEventListener('submit', (e) => {
+  // prevents the page from refreshing
+  e.preventDefault();
+
+});
