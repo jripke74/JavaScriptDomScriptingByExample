491f9a6d2bf5fcce963d4524738a46f1e5d3a718
Jeff Ripke <jripke74@gmail.com>
2017-09-12 06:33:03 -0700

    refractor createLi(text) function

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 
+// this will load HTML first then js
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('registrar');
   const input = form.querySelector('input');
@@ -36,22 +37,25 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   function createLI(text) {
+    function createElement(elementName, property, value) {
+      const element = document.createElement(elementName);
+      element[property] = value;
+      return element
+    }
+
+    function appendToLi(elementName, property, value) {
+      const element = createElement(elementName, property, value)
+      li.appendChild(element);
+      return element;
+    }
+
     const li = document.createElement('li');
-    const span = document.createElement('span');
-    span.textContent = text;
-    li.appendChild(span);
-    const label = document.createElement('label');
-    label.textContent = 'Confirmed';
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    label.appendChild(checkbox);
-    li.appendChild(label);
-    const editButton = document.createElement('button');
-    editButton.textContent = 'edit';
-    li.appendChild(editButton);
-    const removeButton = document.createElement('button');
-    removeButton.textContent = 'remove';
-    li.appendChild(removeButton);
+
+    appendToLi('span', 'textContent', text);
+    appendToLi('label', 'textContent', 'Confirmed').appendChild(createElement('input', 'type', 'checkbox'));
+    appendToLi('button', 'textContent', 'edit')
+    appendToLi('button', 'textContent', 'remove');
+    
     return li;
   }
 
